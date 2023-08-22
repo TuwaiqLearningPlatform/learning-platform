b39ab87cf506837a600213e518b445471d9ee0c0
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-08-22 23:19:32 +0300

    fix ApiException constructor access modifier #13

--- src/main/java/com/example/learningplatform/Api/ApiException/ApiException.java
+++ src/main/java/com/example/learningplatform/Api/ApiException/ApiException.java
@@ -2,7 +2,7 @@ package com.example.learningplatform.Api.ApiException;
 
 public class ApiException extends RuntimeException{
 
-    ApiException(String errMessage){
+    public ApiException(String errMessage){
         super(errMessage);
     }
 }
