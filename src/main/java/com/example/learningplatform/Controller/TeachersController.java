9beeef98ef0dd9a3e67c6d7a572b0f5360994380
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-08-23 10:57:15 +0300

    add teacher CRUD #5

--- src/main/java/com/example/learningplatform/Controller/TeachersController.java
+++ src/main/java/com/example/learningplatform/Controller/TeachersController.java
@@ -1,10 +1,18 @@
 package com.example.learningplatform.Controller;
 
 
+import com.example.learningplatform.Api.ApiResponse.ApiResponse;
+import com.example.learningplatform.Api.ApiResponse.ApiResponseWithMessage;
+import com.example.learningplatform.DTO.TeacherDTO;
+import com.example.learningplatform.Model.Teacher;
 import com.example.learningplatform.Service.TeacherService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/teachers")
@@ -14,5 +22,40 @@ public class TeachersController {
     private final TeacherService teacherService;
 
 
+    // START CRUD
+
+    @GetMapping("/get")
+    public ResponseEntity<ApiResponse<List<Teacher>>> findAll() {
+        return ResponseEntity.ok(new ApiResponse<>(teacherService.findAll()));
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponseWithMessage<Teacher>> addTeacher(@RequestBody @Valid TeacherDTO teacherDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body((new ApiResponseWithMessage<>("the teacher have been created.", teacherService.addTeacher(teacherDTO))));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ApiResponseWithMessage<Teacher>> updateTeacher(@PathVariable Integer id, @RequestBody @Valid TeacherDTO teacherDTO) {
+        return ResponseEntity.ok((new ApiResponseWithMessage<>("the teacher have been updated.", teacherService.updateTeacher(id, teacherDTO))));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ApiResponseWithMessage<Teacher>> deleteTeacher(@PathVariable Integer id) {
+        return ResponseEntity.ok((new ApiResponseWithMessage<>("the teacher have been deleted.", teacherService.deleteById(id))));
+    }
+
+    // END CRUD
+
+    // Search by name
+
+    @GetMapping("/search/name/{name}")
+    public ResponseEntity<ApiResponse<List<Teacher>>> searchByName(@PathVariable String name) {
+        return ResponseEntity.ok((new ApiResponse<>(teacherService.searchForTeachersByName(name))));
+    }
+
+    @GetMapping("/search/id/{id}")
+    public ResponseEntity<ApiResponse<Teacher>> searchByName(@PathVariable Integer id) {
+        return ResponseEntity.ok((new ApiResponse<>(teacherService.findById(id))));
+    }
 
 }
