b1bc64ecad26be8ecbbe115b3a7fe75fe507df3f
Saud ALashrah <76828700+BLANK-13@users.noreply.github.com>
2023-08-23 18:26:25 +0300

    changed some authentication details #27

--- src/main/java/com/example/learningplatform/Controller/LoginController.java
+++ src/main/java/com/example/learningplatform/Controller/LoginController.java
@@ -4,10 +4,7 @@ package com.example.learningplatform.Controller;
 import com.example.learningplatform.Service.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/login")
@@ -17,14 +14,15 @@ public class LoginController {
     private final LoginService loginService;
 
 
-    @GetMapping("/teacher/{username}/{password}")
+    //// post to display sensitive data
+    @PostMapping("/teacher/{username}/{password}")
     public ResponseEntity<?> teacherLogin(@PathVariable String username, @PathVariable String password) {
 
         String token = loginService.teacherLogin(username, password);
         return ResponseEntity.ok("Login successful your token is: " + token);
     }
 
-    @GetMapping("/student/{username}/{password}")
+    @PostMapping("/student/{username}/{password}")
     public ResponseEntity<?> studentLogin(@PathVariable String username, @PathVariable String password) {
 
         String token = loginService.studentLogin(username, password);
