eb2fffc0e5773207faef8632086baa5ce9ea6b85
W2AlharbiMe <w2.alharbi.me@gmail.com>
2023-08-23 17:56:38 +0300

    add student #9

--- src/main/java/com/example/learningplatform/Controller/StudentsController.java
+++ src/main/java/com/example/learningplatform/Controller/StudentsController.java
@@ -1,10 +1,18 @@
 package com.example.learningplatform.Controller;
 
 
+import com.example.learningplatform.Api.ApiResponse.ApiResponse;
+import com.example.learningplatform.Api.ApiResponse.ApiResponseWithMessage;
+import com.example.learningplatform.DTO.StudentDTO;
+import com.example.learningplatform.Model.Student;
 import com.example.learningplatform.Service.StudentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/students")
@@ -13,4 +21,28 @@ public class StudentsController {
 
     private final StudentService studentService;
 
+
+    @GetMapping("/get")
+    public ResponseEntity<List<Student>> findAll() {
+        return ResponseEntity.ok(studentService.findAll());
+    }
+    @GetMapping("/search/id/{id}")
+    public ResponseEntity<Student> findById(Integer id) {
+        return ResponseEntity.ok(studentService.findById(id));
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponseWithMessage<Student>> addStudent(@RequestBody @Valid StudentDTO studentDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body((new ApiResponseWithMessage<>("the student have been created.", studentService.addStudent(studentDTO))));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<ApiResponseWithMessage<Student>> updateStudent(@PathVariable Integer id, @RequestBody @Valid StudentDTO studentDTO) {
+        return ResponseEntity.ok((new ApiResponseWithMessage<>("the student have been updated.", studentService.updateStudent(id, studentDTO))));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ApiResponseWithMessage<Student>> deleteById(@PathVariable Integer id) {
+        return ResponseEntity.ok((new ApiResponseWithMessage<>("the student have been deleted.", studentService.deleteById(id))));
+    }
 }
--- src/main/java/com/example/learningplatform/Service/StudentService.java
+++ src/main/java/com/example/learningplatform/Service/StudentService.java
@@ -1,8 +1,13 @@
 package com.example.learningplatform.Service;
 
+import com.example.learningplatform.Api.ApiException.ApiException;
+import com.example.learningplatform.DTO.StudentDTO;
+import com.example.learningplatform.Model.Student;
 import com.example.learningplatform.Repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,5 +15,75 @@ public class StudentService {
 
 
     private final StudentRepository studentRepository;
+
+
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+    public Student findById(Integer id) {
+        Student student = studentRepository.findStudentById(id);
+
+        if (student == null) {
+            throw new ApiException("student not found");
+        }
+
+        return student;
+    }
+
+    public Student addStudent(StudentDTO studentDTO) {
+        Student student = setStudentAttributes((new Student()), studentDTO);
+
+        uniqueChecks(student);
+
+        studentRepository.save(student);
+
+        return student;
+    }
+
+    public Student updateStudent(Integer id, StudentDTO studentDTO) {
+        Student saved_student = findById(id);
+        String email = saved_student.getEmail();
+        String username = saved_student.getUsername();
+
+        Student student = setStudentAttributes(saved_student, studentDTO);
+
+        student.setEmail(email);
+        student.setUsername(username);
+
+        studentRepository.save(student);
+
+        return student;
+    }
+
+    public Student deleteById(Integer id) {
+        Student student = findById(id);
+
+        studentRepository.deleteById(id);
+
+        return student;
+    }
+
+
+    private Student setStudentAttributes(Student student, StudentDTO studentDTO) {
+        student.setName(studentDTO.getName());
+        student.setEmail(studentDTO.getEmail());
+        student.setPassword(studentDTO.getPassword());
+        student.setUsername(studentDTO.getUsername());
+        student.setBalance(studentDTO.getBalance());
+
+        return student;
+    }
+
+    private void uniqueChecks(Student student) {
+
+        if(studentRepository.checkEmail(student.getEmail()) != null) {
+            throw new ApiException("the email is used.");
+        }
+
+        if(studentRepository.checkUsername(student.getUsername()) != null) {
+            throw new ApiException("the username is used.");
+        }
+    }
 
 }
